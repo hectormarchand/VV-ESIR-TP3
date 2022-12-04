5b473a02698f051a9b4575e31c9829b0b06c157e
Hector Marchand <hector.marchand@orange.fr>
2022-12-04 18:36:31 +0100

    isBalanced() and tests

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,31 @@
 package fr.istic.vv;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class StringUtils {
 
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        return false;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : str.toCharArray()) {
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+            }
+            else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') return false;
+            }
+            else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            }
+            else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+
+
     }
 
 }
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -6,6 +6,48 @@ import static fr.istic.vv.StringUtils.isBalanced;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
+    @Test
+    void test1() {
+        assertTrue(StringUtils.isBalanced(""));
+    }
+    @Test
+    void test2() {
+        assertTrue(StringUtils.isBalanced("([])"));
+    }
 
+    @Test
+    void test3() {
+        assertTrue(StringUtils.isBalanced("{hello   }"));
+    }
+
+    @Test
+    void test4() {
+        assertFalse(StringUtils.isBalanced("["));
+    }
+
+    @Test
+    void test5() {
+        assertFalse(StringUtils.isBalanced("]"));
+    }
+
+    @Test
+    void test6() {
+        assertTrue(StringUtils.isBalanced("hello [[[{ hello } () ]] () ]"));
+    }
+
+    @Test
+    void test7() {
+        assertFalse(StringUtils.isBalanced("({["));
+    }
+
+    @Test
+    void test8() {
+        assertFalse(StringUtils.isBalanced(")]}"));
+    }
+
+    @Test
+    void test9() {
+        assertFalse(StringUtils.isBalanced("(( [] ]))"));
+    }
 
 }
